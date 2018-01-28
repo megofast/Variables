8ee06d1af8c5aace265ae333e4cc8f65719decd6
megofast <33850107+megofast@users.noreply.github.com>
2018-01-28 10:37:04 -0600

    Initial commit

---
+++ variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String args[]) {
+        byte myByte = 2;
+        short myShort = 5;
+        int myInt = 342;
+        long myLong = 743221;
+        float myFloat = (float) 9.8;
+        double myDouble = 24.657;
+        boolean myBool = true;
+        char myChar = 'r';
+
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + myLong);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("boolean: " + myBool);
+        System.out.println("char: " + myChar);
+    }
+}
